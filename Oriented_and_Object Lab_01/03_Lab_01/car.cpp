7e2f386c1b249cf0be6dc6eade944cf1479aea11
hiagopauli <hiagopauli@gmail.com>
2025-03-28 15:22:48 +0100

    Initial commit

--- Oriented_and_Object Lab_01/03_Lab_01/car.cpp
+++ Oriented_and_Object Lab_01/03_Lab_01/car.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <format>
 #include "car.h"
 
 //implementation 
@@ -13,7 +14,7 @@ Car::Car(const std::string& horsepower, const std::string& speed):
 
 //Display
 
-std::string Car::DisplayInfo() const
+/*std::string Car::DisplayInfo() const
 
 {
 	std::string result;
@@ -21,6 +22,21 @@ std::string Car::DisplayInfo() const
 	std::cout << "Horse Power: " << horsepower << std::endl;
 	std::cout << "Max Speed: " << speed << std::endl;
 	return result;
+}*/
+
+std::string Car::DisplayInfo() const
+
+{
+	//std::string result;
+	//result = "Car Database: \n";	
+	//result += ("Horse Power: " + horsepower + "\n";
+	//result += ("Max Speed: " + speed + "\n";
+	
+	return std::format("Car Database:\nHorse Power: {}\nMax Speed: {}\n", horsepower, speed);
+
+	//return result;
 }
+
+
 
 	
--- Oriented_and_Object Lab_01/03_Lab_01/main.cpp
+++ Oriented_and_Object Lab_01/03_Lab_01/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <memory>
 #include "database.h"
 #include "car.h"
 #include "boat.h"
